13a67c88ee4eee05198a94e83514d3ce6f7f3d29
Briana McCloud <bmccloud26122@gmail.com>
2022-04-18 12:44:06 -0400

    commit tree cb5bfee70a602cca0182a0e519f458fb60603b3e

---
+++ index.js
@@ -0,0 +1,8 @@
+function introduction(name) {
+    console.log(name);
+    return `Hi, my name is ${name}.`;
+  }
+  function introductionWithLanguage(name, language) {
+    console.log(name , language);
+        return `Hi, my name is ${name} and I am learning to program in ${language}.``;
+  }
